dfdc90d403514e608e713c98814ccd90df52a07f
seriseyes <bayarkhuu.srs@gmail.com>
2022-10-02 18:59:03 +0800

    password hash error fix

--- models/User.js
+++ models/User.js
@@ -16,6 +16,8 @@ const schema = new mongoose.Schema({
 
 schema.pre("save", async function (next) {
     try {
+        if (!this.isModified('password')) return next();
+
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
         next();
